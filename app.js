a15fc1ce882713b87b6b33859bf20d04e99bedb9
adamabiyuu <advmabiyu@gmail.com>
2026-07-15 23:27:25 +0700

    feat: adding some javascript again :))

--- app.js
+++ app.js
@@ -25,4 +25,54 @@ function addTask() {
     tasks.push(newTask);
     document.getElementById("taskTitle").value = "";
     localStorage.setItem("tasks", JSON.stringify(tasks));
+
+    renderTask();
 }
+
+function renderTask() {
+
+    tasksList.innerHTML = "";
+
+    if(tasks.length === 0) {
+        const li = document.createElement("li");
+        li.classList.add("tasks-list-item");
+        li.innerHTML = `
+            <div class="tasks">
+                <div class="task-title">
+                    Tidak ada kegiatan
+                </div>
+            </div>
+        `;
+        tasksList.appendChild(li);
+        return;
+    }
+    
+    tasks.forEach((task, index) => {
+        const li = document.createElement("li");
+        li.classList.add("tasks-list-item");
+
+        
+        if (task.checked === true) {
+            li.classList.add("checked");
+        }
+
+        li.innerHTML = `
+            <div class="task">
+                <div>
+                    <input type="checkbox" onChange="checkTask(${index})" id="checkTask${index}" ${task.checked ? "checked=true" : ""} />
+                </div>
+                <div class="task-title">${task.title}</div>
+                <button class="button" onclick="deleteTask(${index})">Hapus</button>
+            </div>
+        `;
+
+        li.id = `task${index}`;
+        tasksList.appendChild(li);
+    })
+}
+
+function main() {
+    renderTask();
+}
+
+main();
