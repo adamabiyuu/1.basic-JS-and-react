ed46f85acaf3d421c81fd30dca214cd1be3cd9de
adamabiyuu <advmabiyu@gmail.com>
2026-07-15 23:02:49 +0700

    feat: adding some javascript :)

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-console.log("ini js dari wpu course 2026");
+const tasks = localStorage.getItem("tasks") ? JSON.parse(localStorage.getItem("tasks")) : []; //array
+
+
+const tasksList = document.getElementById("tasksList");
+const taskForm = document.getElementById("taskForm");
+
+taskForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    addTask();
+})
+
+function addTask() {
+    const taskTitleValue = document.getElementById("taskTitle").value;
+
+    if (taskTitleValue.trim() === "") {
+        alert("Judul tidak boleh kosong!");
+        return;
+    }
+
+    const newTask = {
+        id: tasks.length + 1,
+        title: taskTitleValue.trim(),
+    };
+
+    tasks.push(newTask);
+    document.getElementById("taskTitle").value = "";
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
