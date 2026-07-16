7bd5e6df3974fabb8d40d065901bcb9ffd4ef9f8
adamabiyuu <advmabiyu@gmail.com>
2026-07-16 16:26:39 +0700

    fix: correct React and Babel CDN URLs

---
+++ app-react.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+    return (
+        <div>
+            <h1>Belajar React JS di wpu course</h1>
+        </div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("app"));
+root.render(<App />);
