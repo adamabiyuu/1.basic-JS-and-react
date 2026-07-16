fa197abaf92ed3b7cd009fcb2f4f21b1d350843c
adamabiyuu <advmabiyu@gmail.com>
2026-07-16 17:42:42 +0700

    feat: add task form state and local storage persistence

--- app-react.jsx
+++ app-react.jsx
@@ -1,8 +1,61 @@
-const App = () => {
+
+const MyTasksForm = (props) => {
     return (
-        <div>
-            <h1>Belajar React JS di wpu course</h1>
+      <form className="form" id="taskForm" onSubmit={(event) => props.onSubmit(event)}>
+        <div className="form-group">
+          <label htmlFor="taskTitle" className="form-label">
+            Mau ngapain hari ini?
+          </label>
+          <input className="form-input form-input--full" type="text" id="taskTitle" name="taskTitle" placeholder="Masukkan kegiatan" onChange={(event) => props.setTaskTitle(event.target.value)} value={props.taskTitle} />
         </div>
+        <div className="form-group form-group--button-right">
+          <button className="button" type="submit">
+            Simpan
+          </button>
+        </div>
+      </form>
+    );
+}
+
+const MyTasksList = () => {
+    return (
+      <div className="tasks">
+        <h2 className="tasks-title">Agendaku</h2>
+        <ul className="tasks-list" id="tasksList"></ul>
+      </div>
+    );
+}
+
+const MyTasksContainer = (props) => {
+    return <div className="container">{props.children}</div>
+}
+
+const App = () => {
+    const [taskTitle, setTaskTitle] = React.useState("");
+    const [tasks, setTasks] = React.useState([]);
+
+    const addTask = (event) => {
+        event.preventDefault();
+        if(taskTitle.trim() === "") {
+            alert("Judul tidak boleh kosong!");
+            return;
+        }
+
+        const newTask = {
+            id: tasks.length + 1,
+            title: taskTitle.trim(),
+        };
+
+        setTasks([...tasks, newTask]);
+        setTaskTitle("");
+        localStorage.setItem("tasks", JSON.stringify([...tasks, newTask]));
+    }
+    
+    return (
+        <MyTasksContainer>
+            <MyTasksForm onSubmit={addTask} taskTitle={taskTitle} setTaskTitle={setTaskTitle} />
+            <MyTasksList />
+        </MyTasksContainer>
     )
 }
 
