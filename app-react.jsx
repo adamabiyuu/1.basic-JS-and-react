96e5ca72f6660cea755d1dd0aa1a657e73d3bf06
adamabiyuu <advmabiyu@gmail.com>
2026-07-16 21:03:01 +0700

    feat: add task management with local storage and props destructuring

--- app-react.jsx
+++ app-react.jsx
@@ -1,12 +1,13 @@
 
 const MyTasksForm = (props) => {
+    const { onSubmit, setTaskTitle, taskTitle } = props;
     return (
-      <form className="form" id="taskForm" onSubmit={(event) => props.onSubmit(event)}>
+      <form className="form" id="taskForm" onSubmit={(event) => onSubmit(event)}>
         <div className="form-group">
           <label htmlFor="taskTitle" className="form-label">
             Mau ngapain hari ini?
           </label>
-          <input className="form-input form-input--full" type="text" id="taskTitle" name="taskTitle" placeholder="Masukkan kegiatan" onChange={(event) => props.setTaskTitle(event.target.value)} value={props.taskTitle} />
+          <input className="form-input form-input--full" type="text" id="taskTitle" name="taskTitle" placeholder="Masukkan kegiatan" onChange={(event) => setTaskTitle(event.target.value)} value={taskTitle} />
         </div>
         <div className="form-group form-group--button-right">
           <button className="button" type="submit">
@@ -17,11 +18,30 @@ const MyTasksForm = (props) => {
     );
 }
 
-const MyTasksList = () => {
+const MyTasksList = (props) => {
+    const { tasks, deleteTask, checkTask } = props;
     return (
       <div className="tasks">
         <h2 className="tasks-title">Agendaku</h2>
-        <ul className="tasks-list" id="tasksList"></ul>
+        <ul className="tasks-list" id="tasksList">
+          {tasks.length > 0 ? (
+            tasks.map((task, index) => (
+              <li key={task.id} className={`tasks-list-item ${task.checked ? "checked" : ""}`}>
+                <div className="task">
+                  <div>
+                    <input type="checkbox" checked={task.checked} onChange={(event) => checkTask(event, index)} />
+                  </div>
+                  <div className="task-title">{task.title}</div>
+                  <button className="button" onClick={() => deleteTask(index)}>Hapus</button>
+                </div>
+              </li>
+            ))
+          ) : (
+            <div class="tasks">
+              <div className="task-title">Tidak ada kegiatan</div>
+            </div>
+          )}
+        </ul>
       </div>
     );
 }
@@ -33,6 +53,13 @@ const MyTasksContainer = (props) => {
 const App = () => {
     const [taskTitle, setTaskTitle] = React.useState("");
     const [tasks, setTasks] = React.useState([]);
+
+    React.useEffect(() => {
+      const storedTaks = localStorage.getItem("tasks");
+      if (storedTaks) {
+        setTasks(JSON.parse(storedTaks));
+      }
+    }, []);
 
     const addTask = (event) => {
         event.preventDefault();
@@ -51,10 +78,31 @@ const App = () => {
         localStorage.setItem("tasks", JSON.stringify([...tasks, newTask]));
     }
     
+    const deleteTask = (taskIndex) => {
+      const updatedTasks = tasks.filter((_, index) => index !== taskIndex)
+      setTasks(updatedTasks);
+      localStorage.setItem("tasks", JSON.stringify(updatedTasks));
+    };
+
+    const checkTask = (event, taskIndex) => {
+      const isChecked = event.target.checked;
+      const updatedTasks = tasks.map((task, index) => {
+        if (taskIndex === index) {
+          return {
+            ...task,
+            checked: isChecked,
+          };
+        }
+        return task;
+      });
+      setTasks(updatedTasks);
+      localStorage.setItem("tasks", JSON.stringify(updatedTasks));
+    }
+    
     return (
         <MyTasksContainer>
             <MyTasksForm onSubmit={addTask} taskTitle={taskTitle} setTaskTitle={setTaskTitle} />
-            <MyTasksList />
+            <MyTasksList tasks={tasks} checkTask={checkTask} deleteTask={deleteTask} />
         </MyTasksContainer>
     )
 }
